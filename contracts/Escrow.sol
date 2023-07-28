55228127b274492aaf19793a06230024401f4a0b
Vincent <v3rdugo@yahoo.com>
2023-07-28 13:54:28 -0700

    listProperty function (mint Property NFT)

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "./Property.sol";
+
 contract Escrow {
-    address public nftAddress;
+    Property public propertyContract;
     address payable public seller;
     address public legalEntity;
     address public appraiser;
@@ -10,18 +12,25 @@ contract Escrow {
     address public lender;
 
     constructor(
-        address _nftAddress,
+        address _propertyContract,
         address payable _seller,
         address _legalEntity,
         address _appraiser,
         address _inspector,
         address _lender
     ) {
-        nftAddress = _nftAddress;
+        propertyContract = Property(_propertyContract);
         seller = _seller;
         legalEntity = _legalEntity;
         appraiser = _appraiser;
         inspector = _inspector;
         lender = _lender;
     }
+
+    function listProperty(string memory uri) public {
+        // Minting a new NFT in the Property contract implies the listing of the property
+        propertyContract.safeMint(msg.sender, uri);
+    }
+
+    function finalizeSale(uint256 tokenId) external {}
 }
