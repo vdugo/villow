c386130ab9316a0f7901d2ce8b823d8b5a8c2815
Vincent <v3rdugo@yahoo.com>
2023-08-21 23:31:36 -0700

    Bidding.sol boilerplate

--- contracts/Bidding.sol
+++ contracts/Bidding.sol
@@ -1,26 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./Escrow.sol";
 
-contract Bidding is AccessControl {
-    bytes32 public constant SELLER_ROLE = keccak256("SELLER_ROLE");
-    bytes32 public constant BUYER_ROLE = keccak256("BUYER_ROLE");
-
-    struct Bid {
-        address bidder;
-        uint256 amount;
-    }
-
-    mapping(uint256 => Bid) highestBids;
-
-    ERC721 public nftContract;
-
-    constructor(address _nftContract) {
-        _setupRole("DEFAULT_ADMIN_ROLE", msg.sender);
-        nftContract = ERC721(_nftContract);
-    }
-
-    function placeBid(uint256 tokenId) public payable onlyRole(BUYER_ROLE) {}
-}
+contract Bidding {}
